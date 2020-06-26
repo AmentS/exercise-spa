813766bf8bf4b8164890a309bf634d2656035403
Spasoje Djurovic <spasoje.djurovic90@gmail.com>
2020-06-26 22:22:30 +0200

    Started project

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,16 @@
+window._ = require('lodash');
+import Vue from 'vue';
+
+
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
+
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
